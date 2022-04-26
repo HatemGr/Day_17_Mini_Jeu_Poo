68c03b0271ee8eac46a77760c2f1a54836be8eb5
HatemGr <hatemgribi@gmail.com>
2022-04-26 11:03:10 +0200

    Test sur test.rb en ignore

--- test.rb
+++ test.rb
@@ -1,13 +1,13 @@
-# input = gets.chomp
+input = gets.chomp
 
-# case input
-# when /\b\d{1}\b/
-# #when /[0-9]/
-#   puts "Has one digit"
-#   puts input.to_i
-# else
-#   puts "Does not"
-# end
+case input
+when /\b\d{1}\b/
+#when /[0-9]/
+  puts "Has one digit"
+  puts input.to_i
+else
+  puts "Does not"
+end
 
 case 5.to_s
 when /[2346]/
